2303194914fe095317c2a7530807f2005a2d3d21
Théophile Gindre <theophile.gindre@gmail.com>
2016-05-02 15:50:43 +0200

    séparation update_state / draw

--- Pong/Scripts/Pong.lua
+++ Pong/Scripts/Pong.lua
@@ -13,7 +13,7 @@ onInit = function(n, ...)
 	-- ball
     ball_width = 32
     ball_height = 32
-    ball_x = (screen_width / 2) - (ball_width / 2)
+    ball_x = 32--(screen_width / 2) - (ball_width / 2)
     ball_y = (screen_height / 2) - (ball_height / 2)
     ball_speed_x = -(screen_width/6)
     ball_speed_y = (screen_width/6)
@@ -44,7 +44,11 @@ onInit = function(n, ...)
 	tactos_ModifyObject("TEXT",10, tostring(score_j1))
 	-- Message a la fin de la game
 	tactos_AddObject("TEXT", 11, 0, screen_height*0.8, screen_width, 25, "none", "CONFIG:20,CT:black,T:navy: ")
- 
+	
+	-- test
+	tactos_AddObject("TEXT", 12, 10, 10, 200, 25, "none", "CONFIG:20:black,T:navy: ")
+	tactos_AddObject("TEXT", 13, 10, 35, 200, 25, "none", "CONFIG:20:black,T:navy: ")
+	
 	-- Remarque sur les images, la taille des images affichés depend des images elles meme... nul
 	
 	-- Position pour les picots...
@@ -76,72 +80,8 @@ onInit = function(n, ...)
 end
 
 onTimer = function ()
-
--- Déplacement du joueur (avec la souris)
-
-	x,y=tactos_GetTactosPos()
-	paddle_1_y = y - (paddle_1_height/2)
-	
-	-- Le J1 ne peut pas sortir de l'ecran
-	if paddle_1_y <= 0 then -- haut de l'écran
-		paddle_1_y = 0
-	elseif (paddle_1_y + paddle_1_height) >= screen_height then -- bas de l'écran
-		paddle_1_y = screen_height - paddle_1_height
-	end
-	
-	tactos_SetPosObject("IMAGE", 1, paddle_1_x, paddle_1_y, paddle_1_width, paddle_1_height)
-	
--- Mouvements de la balle
-	
-	-- La balle rebondit "en haut" et "en bas"
-	if ball_y <= 0 then
-		ball_speed_y = math.abs(ball_speed_y)
-	elseif (ball_y + ball_height) >= screen_height  then
-		ball_speed_y = -math.abs(ball_speed_y)
-	end
-
-	-- rebondit sur le J1
-	if ball_x <= paddle_1_width and
-		(ball_y + ball_height) >= paddle_1_y and
-		ball_y < (paddle_1_y + paddle_1_height)
-	then
-		ball_speed_x = math.abs(ball_speed_x)
-		score_j1 = score_j1 + 1
-		tactos_ModifyObject("TEXT",10, tostring(score_j1))
-	end
-
-	-- rebondit sur le mur
-	if (ball_x + ball_width) >= (screen_width - mur_1_width) and
-		(ball_y + ball_height) >= mur_1_y and
-		ball_y < (mur_1_y + mur_1_height)
-	then
-		ball_speed_x = -math.abs(ball_speed_x)
-	end
-
-	-- Sortie de balle 
-    if ball_x + ball_width < 0 or ball_x > screen_width then
-		lance_balle = false
-		
-		-- Replacement de la balle au centre
-		ball_x = (screen_width / 2) - (ball_width / 2)
-		ball_y = (screen_height / 2) - (ball_height / 2)
-		ball_speed_x = -(screen_width/6)
-		ball_speed_y = (screen_width/6)
-		
-		-- Affichage du score établie
-		tactos_ModifyObject("TEXT",11, affiche_score .. tostring(score_j1))
-		
-    end
-
-	if lance_balle == true then
-		-- Déplacement de la balle
-		ball_x = ball_x + (ball_speed_x * coeff_v)
-		ball_y = ball_y + (ball_speed_y * coeff_v)
-	end
-	
-	tactos_SetPosObject("IMAGE", 2, ball_x, ball_y, ball_width, ball_height)
-	--tactos_Redraw()
-
+	update_state()
+	draw()
 end
 
 onKey = function (cmd, ch)
@@ -159,61 +99,81 @@ onKey = function (cmd, ch)
 end
 
 
-
--- LA PARTIE EN DESSOUS N EST PAS ENCORE TESTEE !!!
-
-
-
--- TRouve la position de la balle par rapport au terrain et à la raquette
-function pos_balle_devant_r(raquette)
-	-- Si joueur 1
-	if raquette == 1 then
-		p = 0
-		r = 0
-		-- si la balle se situe devant la raquette
-		if (ball_y + ball_height) >= paddle_1_y and
-			ball_y < (paddle_1_y + paddle_1_height) then
-			
-			-- position de la balle par rapport au terrain(4sections)
-			if (ball_x + ball_width) >= p0 and ball_x <= p1 then
-				p = 1
-			elseif (ball_x + ball_width) >= p1 and ball_x <= p2 then
-				p = 2
-			elseif (ball_x + ball_width) >= p2 and ball_x <= p3 then
-				p = 3
-			elseif (ball_x + ball_width) >= p3 and ball_x <= p4 then
-				p = 4
-			end
-			
-			-- position de la balle par rapport à la raquette :
-			r0 = paddle_1_y
-			r1 = paddle_1_y + paddle_1_height/4
-			r2 = paddle_1_y + paddle_1_height/2
-			r3 = paddle_1_y + paddle_1_height*(3/4)
-			r4 = paddle_1_y + paddle_1_height
-			
-			if (ball_y + ball_height) >= r0 and ball_y <= r1 then
-				r = 1
-			elseif (ball_y + ball_height) >= r1 and ball_y <= r2 then
-				r = 2
-			elseif (ball_y + ball_height) >= r2 and ball_y <= r3 then
-				r = 3
-			elseif (ball_y + ball_height) >= r3 and ball_y <= r4 then
-				r = 4
-			end
-			
-		else
-			-- position inutile (aucun picot ne doit etre levé)
-			p = 0
-			r = 0
-		end
+function update_state()
+-- update de la raquette
+	x,y = tactos_GetTactosPos()
+	paddle_1_y = y - (paddle_1_height / 2)
+	
+	if paddle_1_y <= 0 then -- haut de l'écran
+		paddle_1_y = 0
+	elseif (paddle_1_y + paddle_1_height) >= screen_height then -- bas de l'écran
+		paddle_1_y = screen_height - paddle_1_height
 	end
+	
+-- update de la balle
+	if lance_balle == true then
+		ball_x = ball_x + (ball_speed_x * coeff_v)
+		ball_y = ball_y + (ball_speed_y * coeff_v)
+	end
+
+-- update des infos sur la localisation de la balle
+	p = math.ceil(ball_x / (screen_width / 4))
+	r = math.ceil((ball_y - paddle_1_y + ball_height) / ((ball_height + paddle_1_height) / 4))
+	if r < 0 or r > 4 then 
+		r = 0 
+	end
+	
+-- rebonds de la balle
+	--rebond haut et bas
+	if ball_y <= 0 or (ball_y + ball_height) >= screen_height then
+		ball_speed_y = -1 * ball_speed_y
+	end
+
+	-- rebond sur la raquette
+	if ball_x <= (paddle_1_x + paddle_1_width) and r ~= 0 then
+		ball_speed_x = -1 * ball_speed_x
+		ball_x = (paddle_1_x + paddle_1_width)
+		score_j1 = score_j1 + 1
+	end
+
+	-- rebond sur le mur
+	if (ball_x + ball_width) >= (screen_width - mur_1_width) then
+		ball_speed_x = -1 * ball_speed_x
+	end
+	
+-- Sortie de la balle 
+    if ball_x < 0 then
+		lance_balle = false
+		
+		-- Replacement de la balle au centre
+		ball_x = (screen_width / 2) - (ball_width / 2)
+		ball_y = (screen_height / 2) - (ball_height / 2)
+		ball_speed_x = -(screen_width/6)
+		ball_speed_y = (screen_width/6)		
+    end
+end
+
+function draw()
+-- affichage score
+	tactos_ModifyObject("TEXT",10, tostring(score_j1))
+	
+-- affichage score final
+	if lance_balle == false then
+		tactos_ModifyObject("TEXT",11, affiche_score .. tostring(score_j1))
+	end
+	
+-- affichage raquette
+	tactos_SetPosObject("IMAGE", 1, paddle_1_x, paddle_1_y, paddle_1_width, paddle_1_height)
+	
+-- affichage balle	
+	tactos_SetPosObject("IMAGE", 2, ball_x, ball_y, ball_width, ball_height)
+	
+-- test
+	tactos_ModifyObject("TEXT",12, "p : " .. p)
+	tactos_ModifyObject("TEXT",13, "r : " .. r)
 end
 
 
-function Mise_a_jour_picots(x,y)
-
-end
 
 
 
@@ -250,4 +210,4 @@ onKey = function (cmd, ch)
 		--tactos_Redraw(0, screen_height, paddle_1_y, screen_height)		
 	end
 end
-]]
+]]--
