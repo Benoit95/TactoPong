bd84818faaed7ce40acac250e673c978802009c6
Simon Hemi <simon.hemi@etu.utc.fr>
2016-05-04 15:29:40 +0200

    Change tree of Pong folder

--- Pong/Pong/Scripts/Pong.lua
+++ Pong/Pong/Scripts/Pong.lua
@@ -1,5 +1,6 @@
 
 onInit = function(n, ...)
+
 	-- Ecran
     screen_width = 1024
     screen_height = 768
@@ -9,7 +10,7 @@ onInit = function(n, ...)
 	tactos_AddObject("AREA", 21, screen_width/4, 0, screen_width/4, screen_height, "none", "")
 	tactos_AddObject("AREA", 22, screen_width/2, 0, screen_width/4, screen_height, "none", "")
 	tactos_AddObject("AREA", 23, screen_width*(3/4), 0, screen_width/4, screen_height, "none", "")
-	
+
 	-- ball
     ball_width = 32
     ball_height = 32
@@ -18,7 +19,7 @@ onInit = function(n, ...)
 	ball_angle = math.pi -- angle en radians (/!\ : doit toujours etre entre 0 et 2*pi)
 	angle_limit = 0.5 -- limite verticale pour l'angle (1 : pas de limite / 0.1 : très limité)
 	coeff_v = 8 -- coef vitesse de la balle
-	
+
 	-- Raquette 1
     paddle_1_width = 30
     paddle_1_height = 134
@@ -30,41 +31,41 @@ onInit = function(n, ...)
     mur_1_height = screen_height
     mur_1_x = screen_width - mur_1_width
     mur_1_y = 0
-	
+
 	-- Raquette 1
 	tactos_AddObject("IMAGE", 1, paddle_1_x, paddle_1_y, paddle_1_width, paddle_1_height, "none", "Raquette.png")
-	-- Balle 
+	-- Balle
 	tactos_AddObject("IMAGE", 2, ball_x, ball_y, ball_width, ball_height, "none", "Balle.bmp")
 	-- Mur
-	tactos_AddObject("IMAGE", 3, mur_1_x, mur_1_y, mur_1_width, mur_1_height, "none", "Raquette2.png")	
+	tactos_AddObject("IMAGE", 3, mur_1_x, mur_1_y, mur_1_width, mur_1_height, "none", "Raquette2.png")
 	-- Score
 	tactos_AddObject("TEXT", 10, 0, 50, screen_width, 250, "none", "CONFIG:50,CT:black,T:navy:CenterTop")
 	tactos_ModifyObject("TEXT",10, tostring(score_j1))
 	-- Message a la fin de la game
 	tactos_AddObject("TEXT", 11, 0, screen_height*0.8, screen_width, 25, "none", "CONFIG:20,CT:black,T:navy: ")
-	
+
 	-- test
 	tactos_AddObject("TEXT", 12, 10, 10, 200, 25, "none", "CONFIG:20:black,T:navy: ")
 	tactos_AddObject("TEXT", 13, 10, 35, 200, 25, "none", "CONFIG:20:black,T:navy: ")
-	
+
 	-- Remarque sur les images, la taille des images affichés depend des images elles meme... nul
-	
+
 	-- Coordonées du picot à lever
 	p = 0
 	r = 0
 	changeStim = true
-	
+
 	-- score
 	score_j1 = 0
 	affiche_score = "Vous avez fait : "
-	
+
 	-- lancer balle
 	lance_balle = false
-	
+
 	tactos_Redraw()
-	
+
 	tactos_SetTimerValue(20)
-	
+
 	return 20 	-- Timer
 end
 
@@ -77,7 +78,7 @@ onKey = function (cmd, ch)
 	if cmd == 0x0102 then
 		if ch == 32 then	-- SPACE (lance la balle)
 			lance_balle = true
-			
+
 			-- Remise à 0 du score
 			score_j1 = 0
 			tactos_ModifyObject("TEXT",10, tostring(score_j1))
@@ -96,7 +97,7 @@ function update_state()
 	elseif (paddle_1_y + paddle_1_height) >= screen_height then -- bas de l'écran
 		paddle_1_y = screen_height - paddle_1_height
 	end
-	
+
 -- update de la balle
 	if lance_balle == true then
 		ball_x = ball_x + (math.cos(ball_angle) * coeff_v)
@@ -107,34 +108,34 @@ function update_state()
 	local old_p, old_r = p, r
 	p = math.ceil(ball_x / (screen_width / 4))
 	r = math.ceil((ball_y - paddle_1_y + ball_height) / ((ball_height + paddle_1_height) / 4))
-	if r < 0 or r > 4 then 
-		r = 0 
+	if r < 0 or r > 4 then
+		r = 0
 	end
 	changeStim = (old_p ~= p) or (old_r ~= r)
-	
+
 -- rebonds de la balle
 	--rebond haut
 	if ball_y <= 0 then
 		ball_angle = (-1 * ball_angle) % (2 * math.pi)
 		ball_y = 0
 	end
-	
+
 	--rebond bas
 	if (ball_y + ball_height) >= screen_height then
 		ball_angle = (-1 * ball_angle) % (2 * math.pi)
 		ball_y = screen_height - ball_height
 	end
-	
+
 	-- rebond sur le mur
 	if (ball_x + ball_width) >= (screen_width - mur_1_width) then
 		ball_angle = (math.pi - ball_angle) % (2 * math.pi)
 		ball_x = screen_width - mur_1_width - ball_width
 	end
-	
+
 	--rebond sur la raquette
 	rebond_raquette_1()
-	
--- Sortie de la balle 
+
+-- Sortie de la balle
     if ball_x < 0 then
 		lance_balle = false
 		init_ball_pos()
@@ -147,11 +148,11 @@ function rebond_raquette_1()
 		Y = ball_y - paddle_1_y + ball_height
 		H = paddle_1_height + ball_height
 		Z = angle_limit*math.pi/2
-		
+
 		ball_angle = (((Y/H) * (-angle_limit*math.pi)) + Z) % (2 * math.pi)
 		ball_x = (paddle_1_x + paddle_1_width + 1)
 		score_j1 = score_j1 + 1
-	end	
+	end
 end
 
 -- prise en compte de l'angle d'arrivée
@@ -160,11 +161,11 @@ function rebond_raquette_2()
 		Y = ball_y - paddle_1_y + ball_height
 		H = paddle_1_height + ball_height
 		Z = ((2 + angle_limit)*math.pi - 2*ball_angle) / 4
-		
+
 		ball_angle = (((Y/H) * (-angle_limit*math.pi)/2) + Z) % (2 * math.pi)
 		ball_x = (paddle_1_x + paddle_1_width + 1)
 		score_j1 = score_j1 + 1
-	end	
+	end
 end
 
 function init_ball_pos()
@@ -195,22 +196,22 @@ end
 function draw()
 -- affichage score
 	tactos_ModifyObject("TEXT",10, tostring(score_j1))
-	
+
 -- affichage score final
 	if lance_balle == false then
 		tactos_ModifyObject("TEXT",11, affiche_score .. tostring(score_j1))
 	end
-	
+
 -- affichage raquette
 	tactos_SetPosObject("IMAGE", 1, paddle_1_x, paddle_1_y, paddle_1_width, paddle_1_height)
-	
--- affichage balle	
+
+-- affichage balle
 	tactos_SetPosObject("IMAGE", 2, ball_x, ball_y, ball_width, ball_height)
-	
+
 -- test
 	tactos_ModifyObject("TEXT",12, "p : " .. p)
 	tactos_ModifyObject("TEXT",13, "r : " .. r)
-	
+
 -- picots
 	if (changeStim == true) then
 		tactos_SetStim(3, getStimString())
@@ -235,17 +236,17 @@ onKey = function (cmd, ch)
 		elseif ch == 112 then -- p (pause?)
 
 		end
-		
+
 		-- Le J1 ne peut pas sortir de l'ecran
 		if paddle_1_y <= 0 then -- haut de l'écran
 			paddle_1_y = 0
 		elseif (paddle_1_y + paddle_1_height) >= screen_height then -- bas de l'écran
 			paddle_1_y = screen_height - paddle_1_height
 		end
-		
+
 		-- Mise à jour de la raquette 1
 		tactos_SetPosObject("IMAGE", 1, paddle_1_x, paddle_1_y, paddle_1_width, paddle_1_height)
-		--tactos_Redraw(0, screen_height, paddle_1_y, screen_height)		
+		--tactos_Redraw(0, screen_height, paddle_1_y, screen_height)
 	end
 end
 ]]--
